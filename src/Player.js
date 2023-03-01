61e5f9afbc0e105bf263b1d04580e93bbbe2fc4b
Sean Eric Thomas <set@seanericthomas.com>
2023-02-28 18:07:25 -0800

    Make random attack against human player

--- src/Player.js
+++ src/Player.js
@@ -1,17 +1,37 @@
-export default function Player() {
-  const attack = (gameboard, coords) => {
-    const keys = Object.keys(gameboard.board);
-    for (let i = 0; i < keys.length; i += 1) {
-      const current = gameboard.board[keys[i]];
-      for (let j = 0; j < current.coord.length; j += 1) {
-        if (
-          current.coord[j][0] === coords[0]
-          && current.coord[j][1] === coords[1]
-        ) {
-          gameboard.receiveAttack(coords);
-        }
-      }
+function alreadyTried(coord, board) {
+  const { hits } = board;
+  const { misses } = board;
+
+  for (let i = 0; i < hits.length; i += 1) {
+    if (hits[i][0] === coord[0] && hits[i][1] === coord[1]) {
+      return true;
     }
+  }
+
+  for (let i = 0; i < misses.length; i += 1) {
+    if (misses[i][0] === coord[0] && misses[i][1] === coord[1]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function getComputerChoice(board) {
+  const tryCoord = [
+    Math.floor(Math.random() * 10),
+    Math.floor(Math.random() * 10),
+  ];
+  while (alreadyTried(tryCoord, board)) {
+    tryCoord[0] = Math.floor(Math.random() * 10);
+    tryCoord[1] = Math.floor(Math.random() * 10);
+  }
+  return tryCoord;
+}
+
+export default function Player() {
+  const attack = (gameboard, coords = getComputerChoice(gameboard)) => {
+    gameboard.receiveAttack(coords);
   };
 
   return { attack };
--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -8,4 +8,16 @@ test('Player sends attack to other gameboard', () => {
   player.attack(enemyboard, [1, 3]);
   expect(enemyboard.hits.length).toBe(1);
   expect(enemyboard.hits[0]).toStrictEqual([1, 3]);
+  player.attack(enemyboard, [8, 8]);
+  expect(enemyboard.misses.length).toBe(1);
+  expect(enemyboard.misses[0]).toStrictEqual([8, 8]);
+});
+
+test('Computer makes move against human', () => {
+  const playerboard = Gameboard();
+  playerboard.placeShip(3, [1, 3], 'vert');
+  const computer = Player();
+  expect(playerboard.hits.length && playerboard.misses.length).toBe(0);
+  computer.attack(playerboard);
+  expect(playerboard.hits.length || playerboard.misses.length).toBe(1);
 });
